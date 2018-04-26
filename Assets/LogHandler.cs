ead3d9dc75196612caf6f3998f685535724cd837
suzuke <suzuke@hotmail.com>
2018-04-26 23:15:53 +0800

    add test scene

---
+++ Assets/LogHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LogHandler : MonoBehaviour
+{
+    private StreamWriter _writer;
+    void Awake()
+    {
+        _writer = File.AppendText("log.txt");
+        _writer.Write("\n\n=============== Game started ================\n\n");
+        DontDestroyOnLoad(gameObject);
+        Application.logMessageReceived += HandleLog;
+    }
+
+    private void HandleLog(string condition, string stackTrace, LogType type)
+    {
+        var logEntry = string.Format("\n{0} {1:f8}" , Time.frameCount, condition);
+        _writer.Write(logEntry);
+    }
+
+    void OnDestroy()
+    {
+        _writer.Close();
+        Application.logMessageReceived -= HandleLog;
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TrueSync;
+
+public class Test : TrueSyncBehaviour
+{
+    [SerializeField]
+    private int xSize = 5;
+    [SerializeField]
+    private int ySize = 5;
+    [SerializeField]
+    private int zSize = 5;
+
+    [SerializeField]
+    private FP xGap = 0.1;
+    [SerializeField]
+    private FP yGap = 0.1;
+    [SerializeField]
+    private FP zGap = 0.1;
+
+    private FP boxWidth = 1;
+
+    [SerializeField]
+    private GameObject obj;
+
+    private List<GameObject> objStack = new List<GameObject>();
+
+    private int frame = 0;
+
+    public override void OnSyncedStart()
+    {
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+                for (int k = 0; k < zSize; k++)
+                {
+                    GameObject tObj = TrueSyncManager.SyncedInstantiate(obj, new TSVector(
+                        (i - 1) * (boxWidth + xGap),
+                        (j - 1) * (boxWidth + yGap), 
+                        (k - 1) * (boxWidth + zGap)), TSQuaternion.identity);
+                    TSMaterial tMaterial = tObj.AddComponent<TSMaterial>();
+                    tMaterial.restitution = 0;
+                    tMaterial.friction = 1;
+                    objStack.Add(tObj);
+                }
+            }
+        }
+    }
+
+    public override void OnSyncedUpdate()
+    {
+        frame++;
+        int objSize = objStack.Count;
+        FP positionCheckSum = FP.Zero;
+        FP rotationCheckSum = FP.Zero;
+        for (int i=0; i<objSize; i++)
+        {
+            GameObject obj = objStack[i];
+            TSTransform tTransform = obj.GetComponent<TSTransform>();
+            positionCheckSum += (tTransform.position.x + tTransform.position.y + tTransform.position.z);
+            rotationCheckSum += (tTransform.rotation.x + tTransform.rotation.y + tTransform.rotation.z + tTransform.rotation.w);
+        }
+
+        Debug.Log("frame :" + frame + " positionCheckSum: " + positionCheckSum.ToString("F8") + " rotationCheckSum: " + rotationCheckSum.ToString("F8"));
+    }
+
+    public void OnSyncedCollisionEnter(TSCollision collision)
+    {
+        Debug.Log("");
+    }
+}
